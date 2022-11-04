680d932a0ee659a13b36cb0825962fce008fae1d
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-11-04 19:52:09 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string x = "";
+
+        if(strs.size()==0){
+            return "";
+        }
+        else if(strs.size()==1){
+            return strs[0];
+        }
+        for(int i= 0;i<strs[0].size();i++)
+            {
+
+             char t= strs[0][i];
+                for(int j=1;j<strs.size();j++)
+                {
+                    if(t!=strs[j][i])
+                        {
+                        return x;
+
+                        }
+
+                }
+                 x =x + t;
+
+        }
+        return x;
+    }
+};
